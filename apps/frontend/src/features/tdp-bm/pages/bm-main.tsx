c3ea5273db361f38b9cf84684a36584f2a69dd83
sufiyaarab <152899423+sufiyaarab@users.noreply.github.com>
2025-03-28 20:48:43 -0400

    updated

--- apps/frontend/src/features/tdp-bm/pages/bm-main.tsx
+++ apps/frontend/src/features/tdp-bm/pages/bm-main.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import ContractGrouping from './ContactGrouping'
 
 const BmMain = () => {
 
   return (
     <>
         <h1>Bechmarking Module</h1>
+
+    {/* Render the ContractGrouping component */}
+    <div className="mt-10">
+        <ContractGrouping />
+      </div>
 
     </>
   )
